6654b5d698617b34fc9f8b7bc372ebf065184fa5
jonthemango <jonathan.mongeau@mail.concordia.ca>
2019-10-05 15:15:49 -0400

    Updates

---
+++ precache-manifest.ec3c6cf1f574681fe501ba8cf5fa9931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65868c9b088a05ae9c3e5ecb8f41dfd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da1a8c4e2716738f938f",
+    "url": "/static/css/main.6374c534.chunk.css"
+  },
+  {
+    "revision": "e5089707b0146c1bd4bd",
+    "url": "/static/js/2.b28a0fca.chunk.js"
+  },
+  {
+    "revision": "da1a8c4e2716738f938f",
+    "url": "/static/js/main.12f3152e.chunk.js"
+  },
+  {
+    "revision": "a50a83069ef5796961ee",
+    "url": "/static/js/runtime-main.a65a7fae.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
